323edbf37dc88d46bd3cdcaa675528689a5b13a7
robinhu <hu28@seas.upenn.edu>
2025-10-03 19:36:27 -0400

    function four for Q3

--- H4 for 591/src/CoatExperimentSimulator.java
+++ H4 for 591/src/CoatExperimentSimulator.java
@@ -16,7 +16,6 @@ public class CoatExperimentSimulator {
     public int[] simulateCoatExperiment(int iterations) {
         int[] results = new int[iterations];
         for (int i = 0; i < iterations; i++) {
-            // 正确调用静态方法
             int[] arrangement = RandomOrderGenerator.getRandomOrder(numberOfPeople);
             results[i] = numPplWhoGotTheirCoat(arrangement);
         }
@@ -32,6 +31,11 @@ public class CoatExperimentSimulator {
         }
         return (double) countZero / results.length;
     }
-
-
+    public double answerToQuestionTwo(int[] results) {
+        int sum = 0;
+        for (int result : results) {
+            sum += result;
+        }
+        return (double) sum / results.length;
+    }
 }
