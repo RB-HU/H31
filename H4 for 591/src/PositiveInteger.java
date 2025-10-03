9d77a9f676888c62b291e6cdee37ac84ffe3e3f4
robinhu <hu28@seas.upenn.edu>
2025-10-03 16:15:47 -0400

    third function

--- H4 for 591/src/PositiveInteger.java
+++ H4 for 591/src/PositiveInteger.java
@@ -30,5 +30,24 @@ public class PositiveInteger {
         }
         return sum > num;
     }
+    public boolean isNarcissistic() {
+        if (num < 0) {
+            return false;
+        }
 
+        String numStr = String.valueOf(num);
+        int n = numStr.length();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int digit = Character.getNumericValue(numStr.charAt(i));
+            int p = 1;
+            for (int j = 0; j < n; j++) {
+                p *= digit;
+            }
+            sum += p;
+        }
+
+        return sum == num;
+    }
 }
